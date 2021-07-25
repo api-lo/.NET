5ee63817e3e8e539ca8538a1344938bb73124d16
DAYANNA LIZZET VELEZ ARAUJO <dayanna.velez2017@uteq.edu.ec>
2021-07-24 22:00:26 -0500

    Web Services con .Net

---
+++ API/API/Controllers/ExamenesController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using Dapper;
+using System.Web.Http.Cors;
+
+namespace API.Controllers
+{
+	
+	[Route("api/[controller]")]
+	
+	[ApiController]
+	public class ExamenesController : ControllerBase
+	{
+		private string _connection = "Server=localhost;Port=3306;Database=examenes;Uid=root;Pwd=121298;Pooling=False;";
+		
+		[HttpGet]
+		public IActionResult Get()
+		{
+			{
+				IEnumerable<Models.Examenes> lst = null;
+				using (var db = new MySqlConnection(_connection))
+				{
+					var sql = "SELECT * FROM examenes";
+
+					lst = db.Query<Models.Examenes>(sql);
+				}
+
+				return Ok(lst);
+			}
+		}
+		[HttpPost]
+
+		public IActionResult Insert(Models.Examenes model)
+		{
+			int result = 0;
+			using (var db = new MySqlConnection(_connection))
+			{
+				var sql = "insert into examenes(Nombre,Apellido, FechaNacimiento,DNI, " +
+					"Genero, Leucocitos, Hemoglobina, Hematocrito, Hematies, VCM, HCM, CHCM, " +
+					"RDWCV, Linfocitos, Monocitos, Eosinofilos, Basofilos,VPM, Plaquetas, Diagnosticos)"+" " +
+					"values(@Nombre, @Apellido, @FechaNacimiento,@DNI, @Genero, @Leucocitos, @Hemoglobina," +
+					" @Hematocrito, @Hematies, @VCM, @HCM, @CHCM, @RDWCV, @Linfocitos, @Monocitos, " +
+					"@Eosinofilos, @Basofilos,@VPM, @Plaquetas, @Diagnosticos)";
+					result = db.Execute(sql, model);
+			}
+			return Ok(result);
+		}
+
+		[HttpPut]
+
+		public IActionResult Edit(Models.Examenes model)
+		{
+			int result = 0;
+			using (var db = new MySqlConnection(_connection))
+			{
+				var sql = "UPDATE examenes set Nombre=@Nombre,Apellido=@Apellido, " +
+					"FechaNacimiento=@FechaNacimiento,DNI = @DNI, Genero = @Genero, Leucocitos = @Leucocitos, " +
+					"Hemoglobina = @Hemoglobina, Hematocrito = @Hematocrito, Hematies = @Hematies, " +
+					"VCM = @VCM, HCM = @HCM, " +
+					"CHCM = @CHCM, RDWCV = @RDWCV, " + "Linfocitos = @Linfocitos, Monocitos = @Monocitos, " +
+					"Eosinofilos = @Eosinofilos, Basofilos = @Basofilos, VPM = @VPM, Plaquetas = @Plaquetas, " +
+					"Diagnosticos = @Diagnosticos"+" where ID = @ID";
+					result = db.Execute(sql, model);
+			}
+			return Ok(result);
+		}
+
+	
+	}
+}
---
+++ API/API/Models/Examenes.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class Examenes
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; }
+
+        public string FechaNacimiento { get; set; }
+
+        public string DNI { get; set; }
+
+        public string Genero { get; set; }
+
+        public string Leucocitos { get; set; }
+
+        public string Hemoglobina { get; set; }
+
+        public string Hematocrito { get; set; }
+
+        public string Hematies { get; set; }
+
+        public string VCM { get; set; }
+
+        public string HCM { get; set; }
+
+        public string GHCM { get; set; }
+
+        public string RDWCV { get; set; }
+
+        public string Linfocitos { get; set; }
+
+        public string Monocitos { get; set; }
+        
+        public string Eosinofilos { get; set; }
+
+        public string Basofilos { get; set; }
+
+        public string VPM { get; set; }
+
+        public string Plaquetas { get; set; }
+
+        public string Diagnosticos { get; set; }
+
+
+
+
+
+    }
+}
